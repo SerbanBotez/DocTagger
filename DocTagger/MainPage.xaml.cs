d4ba4dfdfa8ad42d8feed08d3a359175b6c19d9f
SerbanBotez <22662951+SerbanBotez@users.noreply.github.com>
2024-04-04 21:55:03 +0300

    Add new "About" page

--- DocTagger/MainPage.xaml.cs
+++ DocTagger/MainPage.xaml.cs
@@ -11,8 +11,8 @@ public partial class MainPage : ContentPage
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        count++;
-
+        count += 5;
+        
         if (count == 1)
             CounterBtn.Text = $"Clicked {count} time";
         else
